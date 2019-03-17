16e0cfeae59ccc1e24278edf5c92bf2f8a135615
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-03-17 22:30:41 +0800

    Update and rename harbour-clubcode.ts to harbour-clubcode–zh.ts

--- translations/harbour-clubcode–zh.ts
+++ translations/harbour-clubcode–zh.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../qml/pages/HomePage.qml" line="17"/>
         <source>Add a new Code</source>
-        <translation type="unfinished"></translation>
+        <translation>添加新条码</translation>
     </message>
 </context>
 </TS>
